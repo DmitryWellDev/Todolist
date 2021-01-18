c7a0b47c811a5f12007983585269415c9a991f78
DmitryWellDew <dewwell0001@gmail.com>
2021-01-18 22:21:26 +0300

    Updates

---
+++ precache-manifest.d35d6b9ad8b4da778f9b6d5a7e49f49d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ffecb064b04e1feddec354c1aecc8b2",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "d55a2c3b2b5139fa9141",
+    "url": "/Todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "d8104e4a7048e8018f8a",
+    "url": "/Todolist/static/js/2.a037a3b5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Todolist/static/js/2.a037a3b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d55a2c3b2b5139fa9141",
+    "url": "/Todolist/static/js/main.f76a8c7b.chunk.js"
+  },
+  {
+    "revision": "8c9ba947015c2dd63600",
+    "url": "/Todolist/static/js/runtime-main.e8babc40.js"
+  }
+]);
